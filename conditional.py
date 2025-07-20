ee4d794ee1ad3387c734c0825c4360602291dc8e
hamdan123-cloud <sheikh13366@hs-spartan.com>
2025-07-20 13:57:42 +0500

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,10 @@
+number = 2
+
+if number > 5:
+    print("Let's check")
+    print('Number is greater than 5')
+
+print("I am inside if condition")
+
+if number < 5:
+    print("The number is less than 5")
