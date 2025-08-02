be1b55639d0587986f24a8a8a0e66799d08c6231
hamdan123-cloud <sheikh13366@hs-spartan.com>
2025-08-02 14:14:29 +0500

    Add files via upload

---
+++ stringing.py
@@ -0,0 +1,17 @@
+string = input("Please enter your own String : ") #but
+string2 = ('')
+
+for i in string:
+    string2 = i + string2
+
+print("\nThe Original String = ", string)
+print("The Reversed String = ", string2)
+
+
+string = 'but'
+string2 = ''
+
+for i in 'but':
+    string2 = i + string2
+
+  
